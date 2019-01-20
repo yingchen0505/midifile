04a741bc106e78815e9d3646b3e6c52f5556968f
yingchen <lai.yingchen@u.nus.edu>
2019-01-20 13:26:02 +0800

    added changing directory

--- src-programs/fyp.cpp
+++ src-programs/fyp.cpp
@@ -5,7 +5,8 @@
 #include <cmath>
 #include <map>
 #include <list>
-//#include "MidiCat.h"
+#include <unistd.h>
+//#include <fstream>
 #include "MidiExcerptByBar.h"
 #include "MusicSegment.h"
 
@@ -17,9 +18,9 @@ using namespace smf;
 using namespace music_segment;
 
 
-// Pause (in seconds) between concatenated midi files.
+// pause (in seconds) between concatenated midi files.
 // used with -p option
-double pause = 0.0;  
+double pauseBetweenMidi = 0.0;  
 
 int startBar = 1;	// Use with -s option (inclusive)
 int endBar = -1;	// Use with -e option (inclusive)
@@ -27,6 +28,18 @@ int endBar = -1;	// Use with -e option (inclusive)
 void checkOptions (Options& opts, int argc, char** argv);
 
 int main(int argc, char* argv[]) {
+	// Store current directory so as to return to this before exiting constructor
+	char homeDirectory[1024];
+	getcwd(homeDirectory, sizeof(homeDirectory));
+	
+	char currDirectory[1024];
+	
+	// Change to image input directory
+	chdir(INPUT_PATH);
+	
+	getcwd(currDirectory, sizeof(currDirectory));
+	cout << currDirectory << "\n";
+	/*
 	Options options;
 	checkOptions(options, argc, argv);
 	
@@ -36,10 +49,16 @@ int main(int argc, char* argv[]) {
 	
 	MusicSegment musicSegment(&prep, &mainLoop, NULL, &finalEnd);
 	MidiFile outfile = musicSegment.repeat(150, true, true);
-	outfile.write(cout);
+	outfile.write(cout);*/
 
 	//MidiExcerptByBar midiExcerptByBar;
 	//midiExcerptByBar.run(argc, argv);
+	
+	chdir(homeDirectory);
+	
+	getcwd(currDirectory, sizeof(currDirectory));
+	cout << currDirectory << "\n";
+	
 }
 
 
@@ -49,12 +68,12 @@ int main(int argc, char* argv[]) {
 //
 
 void checkOptions(Options& opts, int argc, char* argv[]) {
-	opts.define("p|pause=i:1",  "Pause (in seconds) between concatenated midi files.");
+	opts.define("p|pause=i:1",  "pause (in seconds) between concatenated midi files.");
 	opts.define("s|start=i:1",  "Starting bar (inclusive)");
 	opts.define("e|end=i:-1",  "Ending bar (inclusive)");
 	opts.process(argc, argv);
 	
-	pause = opts.getInt("pause");
+	pauseBetweenMidi = opts.getDouble("pause");
 	startBar     =  opts.getInt("start");
 	endBar     = opts.getInt("end");
 }
